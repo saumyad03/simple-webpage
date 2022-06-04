9f4fd3d63425146a055dc4b8f595412892fa2f74
saumyad03 <saumyad03@gmail.com>
2022-06-04 01:51:50 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# simple-webpage
+## introduction
+This is a simple webpage. I'm using it to practice using Git and Github.
---
+++ app.js
@@ -0,0 +1,19 @@
+const title = document.querySelector('#title');
+const text = document.querySelector('#text')
+const btn = document.querySelector('#btn');
+const background = document.querySelector('body')
+let mode = 'light';
+
+btn.addEventListener('click', (e) => {
+    if (mode == 'light') {
+        mode = 'dark';
+        title.style.color = 'white';
+        text.style.color = 'white';
+        background.style.backgroundColor = 'black';
+    } else {
+        mode = 'light'
+        title.style.color = 'black';
+        text.style.color = 'black';
+        background.style.backgroundColor = 'white'
+    }
+})
